5236f51984d93463a10e2120d76d63576747930e
Jakob Löhnertz <jakob2102@gmail.com>
2016-08-05 15:37:14 -0400

    Added the function to add tags and categories to items

--- src/js/main.js
+++ src/js/main.js
@@ -5,25 +5,44 @@ var vm = new Vue({
 		images: [{
 			uid: '1',
 			url: './img/unsplash_1.jpg',
+			dateCreated: 'Aug  2 17:26',
 			tags: ['rose', 'blume'],
 			categories: ['natur']
 		}, {
 			uid: '2',
 			url: './img/unsplash_2.jpg',
+			dateCreated: 'Jul 30 14:09',
 			tags: ['vogelperspektive', 'landschaft'],
 			categories: ['natur']
 		}, {
 			uid: '3',
 			url: './img/unsplash_3.jpg',
+			dateCreated: 'Aug  4 21:38',
 			tags: ['outdoor', 'lagerfeuer'],
 			categories: ['natur']
 		}],
 		searchQuery: '',
+		inputTag: '',
+		inputCategorie: '',
+		tagsSelected: [],
+		categoriesSelected: [],
 		selectedImages: [],
 		visibleImages: []
 	},
 
 	methods: {
+		addInfo: function(e) {
+			var target = event.target.id.substr(3);
+			for (var i = 0; i < vm.selectedImages.length; i++) {
+				var found = this.images.filter(function(a) {
+					return a.uid == vm.selectedImages[i];
+				});
+				found[0][target.toLowerCase()].push(e);
+			}
+			var emptyThis = "input" + target;
+			vm[emptyThis] = '';
+		},
+
 		setVisibleImages: function() {
 			var visible = document.getElementsByClassName("img-container");
 			this.visibleImages.splice(0, this.visibleImages.length);
